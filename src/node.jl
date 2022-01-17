0e7006c263f65354ab4cfd22a22368781f03efc0
Guillaume Lam <guillaume.lam10100@gmail.com>
2022-01-16 21:45:22 -0500

    [WIP] Implenting davidrmiller/biosim4 algo to get used connections

--- src/node.jl
+++ src/node.jl
@@ -1,12 +1,18 @@
-# tmp struct for building of brain
-# this struct allows us to keep track of connections
 mutable struct Node
     remappedNumber::Int
     numOutputs::Int
     numSelfInputs::Int
     numSensorInORNeurons::Int
 end
+# tmp struct for building of brain
+# this struct allows us to keep track of connections to & from each node
+# helps cut useless connections
 
+#+++++
+# Constructor func
+#+++++
+
+# Iter through connections & track internal neuron using Node struct in dict
 function popNodeDict!(conns::Vector{Gene}, nodeDict::Dict{Int,Node})
     for gene in conns
         if gene.sinkType == 0
@@ -30,6 +36,46 @@ function popNodeDict!(conns::Vector{Gene}, nodeDict::Dict{Int,Node})
 
             node = nodeDict[gene.sourceNum]
             node.numOutputs += 1
+        end
+    end
+end
+
+# Remove all neurons with no outputs or only outputs to itself
+# Removing a node may results in other nodes needing to be removed
+function cullNeurons!(conn::Vector{Gene}, nodeDict::Dict{Int,Node})
+    done = false
+
+    while !done
+        done = true
+
+        nodeDictCpy = copy(nodeDict)
+        #iter thru cpy while editing original
+
+        for (key,node) in nodeDictCpy
+            if node.numOutputs == node.numSelfInputs
+                done = false
+                rmvConnToNeuron!(conn, nodeDict, key)
+                #remove
+                delete!(nodeDict, key)
+            # else
+            #     #incr key of entry in dict
+            #     delete!(nodeDict, key)
+            #     nodeDict[(key+1)] = node
+            end
+        end
+    end
+end
+
+# Remove connections to neuron we are culling
+function rmvConnToNeuron!(conn::Vector{Gene}, nodeDict::Dict{Int,Node}, neuronNum::Int)
+    for gene in conn
+        if gene.sinkType == 0 && gene.sinkNum == neuronNum
+            if gene.sourceType == 0
+                nodeDict[gene.sourceNum].numOutputs -= 1
+            end
+            # deleteat!(conn, )
+        else
+
         end
     end
 end
